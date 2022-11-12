a06fbd96ee8e74b235b82209dcaac4ec99a93611
doovis <dovis323@gmail.com>
2022-11-12 22:58:29 +0000

    task adding functionality added

--- src/Components/Task.jsx
+++ src/Components/Task.jsx
@@ -1,17 +1,22 @@
 import React, { useState, useRef } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import "./Task.css";
 
 const Task = () => {
     const [taskActive, setTaskActive] = useState(false);
     const [pomNum, setPomNum] = useState(1);
+    const [curTask, setCurTask] = useState(0);
+    // Add tasks to local storage
     const [tasklist, setTasklist] = useState([]);
     // const taskRef = useRef(taskActive);
 
     const handleTasks = () => {
-        const taskName = document.getElementsByClassName('task-name');
+        const taskName = document.getElementsByClassName("task-name")[0].value;
+        const taskCount = document.getElementsByClassName("pomodoro-num")[0].value;
+        const newList = tasklist.concat({taskname: taskName, taskcount: taskCount});
+        setTasklist(newList);
 
-        console.log(taskName.value);
-
+        setTaskActive(false);
     }
 
     const pomodoroNum = (e) => {
@@ -32,33 +37,43 @@ const Task = () => {
                 <button><img alt="options" src="https://pomofocus.io/icons/threedots-white.png" /></button>
             </div>
 
-            <div>
-                {tasklist.map((item) => {
-                    console.log(item);
-                })}
-            </div>
+            {tasklist.map((item) => {
+                return (
+                <div 
+                    key={uuidv4()}
+                    className="task-item"
+                    >
+                    <img className='task-item-img' src="https://cdn-icons-png.flaticon.com/512/1008/1008958.png" />
+                    <p className='task-item-p1'>{item.taskname}</p>
+                    <p className='task-item-p2'>{curTask}/{item.taskcount}</p>
+                    <button className='task-item-button'>
+                        <img alt="options" src="https://pomofocus.io/icons/vertical-ellipsis.png" />
+                    </button>
+                </div>
+                )
+            })}
             {
-                taskActive ? 
-                <div className="new-task">
-                    <div className='conf-area'>
-                        <input type="text" className="task-name" placeholder="What are you working on?"/>
-                        <p>Est Pomodoros</p>
-                        <div className="pom-input">
-                            <input className='pomodoro-num' type="text" value={pomNum} onChange={(ev) => {pomodoroNum(ev)}}/>
-                            <button className='pomo-controls' onClick={() => {if (pomNum < 999) setPomNum(Number(pomNum)  + 1)}}>▲</button>
-                            <button className='pomo-controls' onClick={() => {if (pomNum > 1) setPomNum(pomNum  - 1)}}>▼</button>
-                        </div>
-                    </div>
-                    <div className='action-area'>
-                        <button onClick={() => setTaskActive(false)}>Cancel</button>
-                        <button onClick={() => {handleTasks(); setTaskActive(false)}}>Save</button>
+            taskActive ? 
+            <div className="new-task">
+                <div className='conf-area'>
+                    <input type="text" className="task-name" placeholder="What are you working on?"/>
+                    <p>Est Pomodoros</p>
+                    <div className="pom-input">
+                        <input className='pomodoro-num' type="text" value={pomNum} onChange={(ev) => {pomodoroNum(ev)}}/>
+                        <button className='pomo-controls' onClick={() => {if (pomNum < 999) setPomNum(Number(pomNum)  + 1)}}>▲</button>
+                        <button className='pomo-controls' onClick={() => {if (pomNum > 1) setPomNum(pomNum  - 1)}}>▼</button>
                     </div>
                 </div>
-                :
-                <div className='add-task' onClick={() => setTaskActive(true)}>
-                    <img alt="" src="https://pomofocus.io/icons/plus-circle-white.png" />
-                    <p>Add Task</p>
+                <div className='action-area'>
+                    <button onClick={() => setTaskActive(false)}>Cancel</button>
+                    <button onClick={handleTasks}>Save</button>
                 </div>
+            </div>
+            :
+            <div className='add-task' onClick={() => setTaskActive(true)}>
+                <img alt="" src="https://pomofocus.io/icons/plus-circle-white.png" />
+                <p>Add Task</p>
+            </div>
             }
         </div>
     )
